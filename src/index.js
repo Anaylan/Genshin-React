a4411b3b31f85c78c73e00047df31118383baa38
Anaylan <sasha.skobelev1234@outlook.com>
2021-12-26 16:26:40 +0600

    add prepros, connect fonts and create sample

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>
+		<Navbar />
+	</React.StrictMode>,
+	document.getElementById("header")
+)
+
 ReactDOM.render(
-    <React.StrictMode>
-        <Footer />
-    </React.StrictMode>,
-    document.getElementById('footer')
-);
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+)
+
 ReactDOM.render(
-    <React.StrictMode>
-        <Navbar />
-    </React.StrictMode>,
-    document.getElementById('header')
-);
+	<React.StrictMode>
+		<Footer />
+	</React.StrictMode>,
+	document.getElementById("footer")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
