a5043b8bae56700a097af361c4b0c25333bada5c
Anaylan <sasha.skobelev1234@outlook.com>
2021-12-27 13:10:07 +0600

    decomposition
    decomposition (maybe wrong)

---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main id="page" className="h-100">
+            <section
+                className="h-100 bg-white bg-opacity-75 shadow rounded-top mt-lg-4 container-lg container-fluid"
+            >
+                <div className="pt-3" id="content"></div>
+            </section>
+        </main>
+    );
+};
+
+export default Main;
--- src/layouts/LayoutDefault.jsx
+++ src/layouts/LayoutDefault.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
+import Main from "../components/layout/Main";
 
 const LayoutDefault = () => (<>
     <Header/>
-    <main id="page" class="h-100">
-        <section
-            className="h-100 bg-white bg-opacity-75 shadow rounded-top mt-lg-4 container-lg container-fluid"
-        >
-            <div className="pt-3" id="content"></div>
-        </section>
-    </main>
+    <Main/>
     <Footer/>
 </>);
 
