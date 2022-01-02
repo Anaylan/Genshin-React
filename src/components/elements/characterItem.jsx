2d554b209e68443b839bd0e1ca2f8e709a58b01f
John Doe <johndoe@example.com>
2022-01-02 11:55:43 +0600

    create dynamic routing

--- src/components/elements/characterItem.jsx
+++ src/components/elements/characterItem.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import {Image} from "react-bootstrap";
+import {Link, useParams} from 'react-router-dom';
 
 const CharacterItem = (props) => {
-
     console.log(props)
     return (
         <div className={`character border rounded ${props.character.weapon_type} bg-white`}>
-            <a href='#' className={'text-center'}>
+            <Link to={`/wiki/characters/${props.character.id}`} className={'text-center'}>
                 <div className={`card-img  ${props.character.rarity}`}>
                     <Image src={props.character.src} alt={props.character.body}/>
                 </div>
                 <div className={'card-body w-100 fs-8 bg-white text-black'}>
                     {props.character.body}
                 </div>
-            </a>
+            </Link>
         </div>
     );
 };
